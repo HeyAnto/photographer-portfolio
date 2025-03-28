94c1f726776846f6e51a200032ddd7690ee377c6
HeyAnto <antonin63170@gmail.com>
2025-03-28 20:51:35 +0100

    Update Grid Services & Added Commentary

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,10 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // Prend toutes les classes "dropdown"
   const dropdowns = document.querySelectorAll(".dropdown");
 
+  // Parcourt chaque "dropdown"
   dropdowns.forEach((dropdown) => {
+    // Sélectionne le bouton
     const button = dropdown.querySelector(".dropbtn");
 
+    // Au clique
     button.addEventListener("click", () => {
+      // Ajoute ou retire la classe "open" sur le dropdown (pour ouvrir et fermer le menu)
       dropdown.classList.toggle("open");
     });
   });
--- assets/js/hero.js
+++ assets/js/hero.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // Tableau contenant les url des images
   const images = [
     "/assets/img/hero/image-hero-1.webp",
     "/assets/img/hero/image-hero-2.webp",
@@ -9,14 +10,22 @@ document.addEventListener("DOMContentLoaded", () => {
     "/assets/img/hero/image-hero-7.webp",
   ];
 
+  // Démarre à l'index 0 du tableau
   let index = 0;
+
+  // Élément qui servira de fond
   const heroBg = document.querySelector(".hero-background");
 
+  // Fonction pour changer l'image
   function changeImage() {
+    // Modifie l'image de fond en utilisant l'index actuel
     heroBg.style.backgroundImage = `url(${images[index]})`;
+
+    // +1 à l'index et boucle si on dépasse la longueur du tableau
     index = (index + 1) % images.length;
   }
 
   setInterval(changeImage, 3000);
+
   changeImage();
 });
